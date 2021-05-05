e29697f2c548ff20da9d2f63005f0811e1ddc08c
CameronFarquhar <cameronfarquhar@gmail.com>
2021-05-05 12:53:29 -0400

    FLASK sql queries built

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from config import sqlkey
 
 conn = create_engine(f'postgresql://postgres:{sqlkey}@localhost:5432/Money_Ball_DB').connect()
 
-
 #################################################
 # Flask Setup
 #################################################
@@ -30,16 +29,36 @@ def welcome():
     return render_template("Home.html")
 
 
-@app.route("/api/Visuals")
-def names():
+@app.route("/api/Map")
+def map():
     results_champion = pd.read_sql_query("SELECT * FROM champion_table", conn).to_dict()
-    # results_nhl = pd.read_sql_query("SELECT * FROM nhl", conn).to_dict()
     return results_champion
 
-# @app.route("/api/v1.0/Map")
-# def map():
-#     results = pd.read_sql_query("SELECT * FROM champion_table", conn).to_dict()
-#     return results
+@app.route("/api/MLB")
+def mlb():
+    results_mlb = pd.read_sql_query("SELECT * FROM mlb", conn).to_dict()
+    return results_mlb
+
+@app.route("/api/MLS")
+def mls():
+    results_mls = pd.read_sql_query("SELECT * FROM mls", conn).to_dict()
+    return results_mls
+
+@app.route("/api/NBA")
+def nba():
+    results_nba = pd.read_sql_query("SELECT * FROM nba", conn).to_dict()
+    return results_nba
+
+@app.route("/api/NFL")
+def nfl():
+    results_nfl = pd.read_sql_query("SELECT * FROM nfl", conn).to_dict()
+    return results_nfl
+
+@app.route("/api/NHL")
+def nhl():
+    results_nhl = pd.read_sql_query("SELECT * FROM nhl", conn).to_dict()
+    return results_nhl
+
 
 if __name__ == '__main__':
     app.run(debug=True)
